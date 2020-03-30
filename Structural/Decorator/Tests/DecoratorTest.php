56e9235740c217565d106151543ee7aa2d83c5f9
liuyibao <734988964@qq.com>
2020-03-30 17:19:24 +0800

    使用phpunit

--- Structural/Decorator/Tests/DecoratorTest.php
+++ Structural/Decorator/Tests/DecoratorTest.php
@@ -3,7 +3,39 @@
 namespace DesignPatterns\Structural\Decorator\Tests;
 
 
-class DecoratorTest
-{
+use DesignPatterns\Structural\Decorator\DoubleRoomBooking;
+use DesignPatterns\Structural\Decorator\ExtraBed;
+use DesignPatterns\Structural\Decorator\WiFi;
+use PHPUnit\Framework\TestCase;
 
+
+class DecoratorTest extends TestCase
+{
+    public function testBasic()
+    {
+        $doubleRoomBooking = new DoubleRoomBooking();
+
+        $this->assertSame(40, $doubleRoomBooking->calculatePrice());
+        $this->assertSame('double room', $doubleRoomBooking->getDescription());
+    }
+
+    public function testTwo()
+    {
+        $doubleRoomBooking = new DoubleRoomBooking();
+        $extraBed = new ExtraBed($doubleRoomBooking);
+
+        $this->assertSame(70, $extraBed->calculatePrice());
+        $this->assertSame('double room with extra bed', $extraBed->getDescription());
+    }
+
+    public function testThree()
+    {
+        $doubleRoomBooking = new DoubleRoomBooking();
+        $extraBed = new ExtraBed($doubleRoomBooking);
+        $wifi = new WiFi($extraBed);
+
+        $this->assertSame(72, $wifi->calculatePrice());
+        $this->assertSame('double room with extra bed with wifi', $wifi->getDescription());
+
+    }
 }
